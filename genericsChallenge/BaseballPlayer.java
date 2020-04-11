8d649a0d6fdc3e175cec21bdef33c6c988a64879
Shubham Sawarkar <shubhamsawarkar07@gmail.com>
2020-04-11 11:38:05 +0530

    fifth commit

---
+++ genericsChallenge/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package com.genericsChallenge;
+
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ genericsChallenge/FootballPlayer.java
@@ -0,0 +1,8 @@
+package com.genericsChallenge;
+
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
